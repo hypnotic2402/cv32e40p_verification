503fc4f69e5905c262bca635f1a3c23c896b9978
Saket Gurjar <saketgurjar2402@gmail.com>
2023-05-14 12:43:36 +0530

    Add files via upload

--- ver/ff_one/scoreboard.sv
+++ ver/ff_one/scoreboard.sv
@@ -9,6 +9,7 @@ class scoreboard;
     mailbox mon2scb;
     int numTransactions;
     transaction trans;
+  	int flag;
 
     // Constructor
     function new(mailbox mon2scb);
@@ -23,9 +24,22 @@ class scoreboard;
 
             // Chekcing AND operation
           
-
-            $display("time = %0d, SCOREBOARD : No Ones = %0b , First One = %0d , Input = %0b" , $time , trans.cno_ones_o , trans.first_one_o , trans.in_i);
-
+			flag = 0;
+          for (int i = 0 ; i < trans.first_one_o ; i++) begin
+            if (trans.in_i[i] == 1) begin
+              flag = 1;
+              break;
+            end
+          end
+          
+          if ((flag == 0) && (trans.in_i[trans.first_one_o] == 1)) begin
+          
+            $display("time = %0d, SCOREBOARD : CORRECT - No Ones = %0b , First One = %0d , Input = %0b" , $time , trans.cno_ones_o , trans.first_one_o , trans.in_i);
+          end
+              
+          else begin
+            $display("time = %0d, SCOREBOARD : WRONG - No Ones = %0b , First One = %0d , Input = %0b" , $time , trans.cno_ones_o , trans.first_one_o , trans.in_i);
+          end
           
 
             numTransactions++;
